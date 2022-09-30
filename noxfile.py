dbd901c103affb27d8096240e5960047250e5df2
fccoelho <fccoelho@gmail.com>
2022-09-30 10:36:59 -0300

    improved nox tests session

--- noxfile.py
+++ noxfile.py
@@ -1,10 +1,29 @@
 from nox_poetry import session
+from nox_poetry import Session
 from glob import glob
+from pathlib import Path
 
 @session(python=["3.10"])
 def tests(session):
     session.install("pytest", ".")
-    session.run("pytest")
+    session.install("coverage[toml]", "pytest", "pygments")
+    try:
+        session.run("coverage", "run", "--parallel", "-m", "pytest", *session.posargs)
+    finally:
+        if session.interactive:
+            session.notify("coverage", posargs=[])
+
+@session
+def coverage(session: Session) -> None:
+    """Produce the coverage report."""
+    args = session.posargs or ["report"]
+
+    session.install("coverage[toml]")
+
+    if not session.posargs and any(Path().glob(".coverage.*")):
+        session.run("coverage", "combine")
+
+    session.run("coverage", *args)
 
 @session
 def lint(session):
