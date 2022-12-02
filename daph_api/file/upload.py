73b8514aa8739ebf3af28a24a9703268391317e8
Juan Belieni <juanbelieni@gmail.com>
2022-12-02 18:58:26 -0300

    feat: add call to submit manuscript

--- daph_api/file/upload.py
+++ daph_api/file/upload.py
@@ -1,10 +1,10 @@
 from hashlib import md5
 
 def upload_file(file_bytes: bytes):
-    name = md5(file_bytes).hexdigest()
-    path = f"files/{name}.pdf"
+    id = md5(file_bytes).hexdigest()
+    path = f"files/{id}.pdf"
 
     with open(path, "wb") as file:
         file.write(file_bytes)
 
-    return path
+    return id, path
--- daph_api/manuscript/api.py
+++ daph_api/manuscript/api.py
@@ -1,12 +1,19 @@
 from typing import List
-from django.forms import ValidationError
-from ninja import Router, File
+from ninja import Router, File, Form
 from ninja.pagination import paginate, PageNumberPagination
 from registry.models import Author, Manuscript
 from ninja import Query, Schema, ModelSchema
 from ninja.files import UploadedFile
+from django.contrib.auth.models import User
+from registry.models import Author, Manuscript
+from ninja.security import django_auth
 from ..file.upload import upload_file
+from datetime import datetime
+import web3
+
 router = Router()
+
+w3 = web3.Web3(web3.HTTPProvider("http://localhost:8545"))
 
 
 class SearchQuery(Schema):
@@ -18,6 +25,7 @@ class AuthorSchema(ModelSchema):
     class Config:
         model = Author
         model_fields = ["id"]
+
 
 class ManuscriptSchema(ModelSchema):
     authors: List[AuthorSchema] = []
@@ -32,7 +40,29 @@ class ManuscriptSchema(ModelSchema):
 def search(request, query: SearchQuery = Query(...)):
     return Manuscript.objects.filter(title__icontains=query.query).order_by(query.order)
 
-@router.post("/upload")
-def create(request, file: UploadedFile = File(...)):
-    path = upload_file(file.read())
-    return {"file_path": path}
+
+submit_manuscript_contract_abi = [
+    {
+        "inputs": [
+            {"internalType": "string", "name": "idmanuscript", "type": "string"}
+        ],
+        "name": "submit_manuscript",
+        "outputs": [],
+        "stateMutability": "nonpayable",
+        "type": "function",
+    }
+]
+
+
+@router.post("/upload", auth=django_auth)
+def create(request, title: str = Form(...), file: UploadedFile = File(...)):
+    id, path = upload_file(file.read())
+
+    author = Author.objects.get(user_id=request.auth)
+    manuscript = Manuscript.objects.create(title=title, date_submission=datetime.now())
+    manuscript.authors.add(author.id)
+
+    dpublish_contract = w3.eth.contract(address="<DPUPLISH_CONTRACT_ADDRESS", abi=submit_manuscript_contract_abi)
+    tx = dpublish_contract.functions.submit_manuscript(id).transact({"from": author.address})
+
+    return {"file_path": path, "transaction": tx.hex()}
